9fd4d5e570df21749104284ec735b9a6006e00bd
Madhu5A3 <madhuv299@gmail.com>
2020-05-10 16:45:27 +0530

    Foundation Screens Added

--- App.js
+++ App.js
@@ -1,19 +1,162 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './components/HomeScreen';
+import NotificationScreen from './components/NotificationScreen';
+import BellIcon from 'react-bell-icon';
+import ServiceScreen from './components/ServiceScreen';
+import CloudServantDrawer from './components/CloudServantDrawer';
+import CallMeScreen from './components/CallMeScreen';
+import UploadScreen from './components/UploadScreen';
+import PlaceOrderScreen from './components/PlaceOrderScreen';
+import NotificationIconComponent from './components/NotificationIconComponent';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const ServiceScreenStackDrawer = createStackNavigator({
+  ServiceScreen: {
+    screen: ServiceScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Service',
+      headerLeft: () => <CloudServantDrawer navigationProps={navigation} />,
+      headerRight: () => <NotificationIconComponent navigationProps={navigation} />,
+      headerStyle: {
+        backgroundColor: '#FF4136',
+      },
+      // headerRight: <BellIcon navigationProps={navigation}/>
+    }),
+  }
 });
+
+const CallMeScreenStackDrawer = createStackNavigator({
+  CallMeScreen: {
+    screen: CallMeScreen,
+    navigationOptions: ({navigation}) => ({
+      title: 'CallMeScreen',
+      headerStyle:{
+        backgroundColor: '#FF4136',
+      }
+    }),
+    headerRight: () => <NotificationIconComponent navigationProps={navigation} />,
+  }
+})
+
+const PlaceOrderScreenStackDrawer = createStackNavigator({
+  PlaceOrderScreen: {
+    screen: PlaceOrderScreen,
+    navigationOptions: ({navigation}) => ({
+      title: 'Place Order Screen',
+      headerStyle: {
+        backgroundColor: '#FF4136'
+      }
+    }),
+    headerRight: () => <NotificationIconComponent navigationProps={navigation} />,
+  }
+})
+
+const UploadScreenStackDrawer = createStackNavigator({
+  UploadScreen:{
+    screen: UploadScreen,
+    navigationOptions: ({navigation}) => ({
+      title: 'Upload Screen',
+      headerStyle: {
+        backgroundColor: '#FF4136',
+      },
+    }),
+    headerRight: () => <NotificationIconComponent navigationProps={navigation} />,
+  }
+})
+const HomeScreenStackDrawer = createStackNavigator({
+  HomeScreen: {
+    screen: HomeScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Home',
+      headerLeft: () => <CloudServantDrawer navigationProps={navigation} />,
+      headerRight: () => <NotificationIconComponent navigationProps={navigation} />,
+      headerStyle: {
+        backgroundColor: '#FF4136',
+      },
+      headerTintColor: '#fff',
+      // headerRight: <BellIcon navigationProps={navigation}/>
+    }),
+  }
+})
+/*const ScreenStackNavigator = createStackNavigator({
+  ScreenStack: {
+      screen: ServiceScreen,
+      navigationOptions: ({ navigation }) => ({
+          title: 'Service',
+          headerLeft: () => <CloudServantDrawer navigationProps={navigation} />,
+          headerStyle: {
+              backgroundColor: 'red',
+          },
+          // headerRight: <BellIcon navigationProps={navigation}/>
+      }),
+  }
+})*/
+
+const NotificationScreenStackDrawer = createStackNavigator({
+  Notification: {
+    screen: NotificationScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Notifications',
+      headerLeft: () => <CloudServantDrawer navigationProps={navigation} />,
+      headerStyle: {
+        backgroundColor: '#FF4136',
+      },
+    }),
+    headerRight: () => <NotificationIconComponent navigationProps={navigation} />,
+  }
+})
+
+
+const CloudServantStackDrawerNavigator = createDrawerNavigator({
+  HomeScreen: {
+    screen: HomeScreenStackDrawer,
+    navigationOptions: {
+      drawerLabel: 'Home Label',
+    },
+  },
+  NotificationScreen: {
+    screen: NotificationScreenStackDrawer,
+    navigationOptions: {
+      drawerLabel: 'Notification Label',
+    },
+  },
+  ServiceScreen: {
+    screen: ServiceScreenStackDrawer,
+    navigationOptions: {
+      drawerLabel: 'Service Label'
+    }
+  },
+  CallMeScreen:{
+    screen: CallMeScreenStackDrawer,
+    navigationOptions: {
+      drawerLabel: 'Call Me Screen Label'
+    }
+  },
+  UploadScreen:{
+    screen: UploadScreenStackDrawer,
+    navigationOptions:{
+      drawerLabel: 'Upload Screen Label'
+    }
+  },
+  PlaceOrderScreen:{
+    screen: PlaceOrderScreenStackDrawer
+  }
+},
+);
+
+/*const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row'
+  },
+  imageContainer: {
+    width: 25, height: 25, marginLeft: 5
+  }
+})*/
+
+// const App = createAppContainer(CloudServantStack);
+// export default App;
+const AppDrawer = createAppContainer(CloudServantStackDrawerNavigator);
+export default AppDrawer;
---
+++ components/NotificationIconComponent.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import BellIcon from 'react-bell-icon';
+
+class NotificationIconComponent extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={require('../assets/bell-icon-2.png')}
+                    style={styles.IconContainer}
+                />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row'
+    },
+    IconContainer: {
+        width: 25, height: 25, marginRight: 7
+    }
+})
+export default NotificationIconComponent;
