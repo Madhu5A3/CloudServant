d1017a2ee574f86c9c2ad02684c3c0a89c8dc3f9
Madhu5A3 <madhuv299@gmail.com>
2020-05-14 23:16:17 +0530

    Foundation Screens: Service,Upload,PlaceOrder,Order,CallMe

---
+++ CloudServant/components/PlaceOrderScreen.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { View, StyleSheet, Text, Button } from 'react-native';
+import UploadCard from './UploadCard';
+import { TextInput } from 'react-native-gesture-handler';
+// import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+import t from 'tcomb-form-native'; // 0.6.9
+
+const Form = t.form.Form;
+
+const UserData = t.struct({
+    title: t.String,
+    orderbrief: t.String
+});
+
+const PlaceOrderScreen = ({ navigation }) => {
+    return (
+        <View style={styles.screen}>
+            <View style={styles.formContainer}>
+                <UploadCard style={styles.inputContainer}>
+                    <View style={styles.textInputContainer}>
+                        <TextInput placeholder="Enter the title of order" />
+                    </View>
+                    <View style={styles.textInputContainer}>
+                        <TextInput placeholder="Description" multiline={true} numberOfLines={5} />
+                    </View>
+                </UploadCard>
+            </View>
+            <View style={styles.buttonContainer}>
+                <View style={styles.button}>
+                    <Button title="BACK" onPress={() => navigation.navigate('ServiceScreen')}></Button>
+                </View>
+                <View style={styles.button}>
+                    <Button title="SUBMIT" onPress={() => { }}></Button>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    OrderContainer: { padding: 10, flexDirection: 'column', justifyContent: 'space-between', marginTop: 20},
+    screen: { padding: 20, flex: 1, alignItems: 'center', marginTop: 10 },
+    inputContainer: { width: 400, maxWidth: '80%', alignItems: 'center', justifyContent: 'space-between', margin: 30, height: 200, },
+    buttonContainer: {
+        flexDirection: 'row', width: '80%', justifyContent: 'space-between'
+    },
+    button: { width: 100 },
+    formContainer: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        marginTop: 10
+    },
+    textInputContainer: {
+        textAlignVertical: 'top'
+    }
+});
+
+export default PlaceOrderScreen;
---
+++ CloudServant/components/ServiceScreen.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, StyleSheet, Text, Button } from 'react-native';
+import UploadCard from './UploadCard';
+
+const ServiceScreen = ({ navigation }) => {
+    return (
+        <View style={styles.homeScreen}>
+            <Text style={styles.textContainer}>Types of Requests</Text>
+            <UploadCard style={styles.homeContainer}>
+                <View style={styles.buttonContainer}>
+                    <Button title="CALL ME" onPress={() => navigation.navigate('CallMeScreen')}></Button>
+                    <Button title="UPLOAD" onPress={() => navigation.navigate('UploadScreen')}></Button>
+                    <Button title="PLACE ORDER" onPress={() => navigation.navigate('PlaceOrderScreen')}></Button>
+                </View>
+            </UploadCard>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    homeContainer: {
+        width: 300, maxWidth: '80%', alignItems: 'center'
+    },
+    homeScreen: {
+        flex: 1,
+        paddingTop: 20,
+        alignItems: 'center',
+        marginTop: 50,
+        justifyContent: 'center'
+    },
+    textContainer: {
+        fontSize: 20,
+        marginTop: 10
+    },
+    buttonContainer: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        width: 200,
+        height: 200,
+        marginVertical: 50,
+        marginTop: 100
+    }
+})
+
+export default ServiceScreen;
